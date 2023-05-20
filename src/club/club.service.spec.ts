f0e8ec59b369fbc2f4b6a26fed8a73b2b3b3d4f9
Yeongil Yoon <askme14397@gmail.com>
2023-05-20 12:22:55 +0900

    Update club service test

--- src/club/club.service.spec.ts
+++ src/club/club.service.spec.ts
@@ -1,3 +1,23 @@
+import { Test } from '@nestjs/testing';
+import { mockDeep } from 'jest-mock-extended';
+import { PrismaService } from 'src/prisma/prisma.service';
+import { StorageService } from 'src/storage/storage.service';
+import { ClubService } from './club.service';
+
 describe('ClubService', () => {
+  beforeEach(async () => {
+    const moduleRef = await Test.createTestingModule({
+      providers: [ClubService],
+    })
+      .useMocker((token) => {
+        switch (token) {
+          case PrismaService:
+            return mockDeep<PrismaService>();
+          case StorageService:
+            return mockDeep<StorageService>();
+        }
+      })
+      .compile();
+  });
   it('always true', () => {});
 });
